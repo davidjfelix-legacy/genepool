1201dccaf7201e9ce37f08ac9f1668c449037a8a
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-09-21 22:42:13 -0400

    Create __init__.py

---
+++ golang/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# This file is intentionally blank.
+# It's used by Python as a breadcrumb for determining where the boundaries
+# of a module are, in relation to the file system.
