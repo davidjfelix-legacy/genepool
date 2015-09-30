454d96d7f6fad6d881399ddcbe414ba1e482743d
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-09-30 14:59:00 -0400

    Add packages and add runners for brew (unused)

--- ubuntu-monolith.py
+++ ubuntu-monolith.py
@@ -6,10 +6,50 @@ import os
 def apt_install(packages):
   env = os.environ.copy()
   env[DEBIAN_FRONTEND] = "noninteractive"
-  subprocess.call('sudo -E apt-get update')
-  subprocess.call('sudo -E apt-get install -y ' + ' '.join(packages))
+  subprocess.call(['sudo', '-E', 'apt-get', 'update')
+  subprocess.call(['sudo', '-E', 'apt-get', '-y', 'install'] + packages)
+  
+def brew_install(packages):
+  subprocess.call(['brew', 'update'])
+  subprocess.call(['brew', 'install'] + packages)
+
+def brew_cask_install(packages):
+  subprocess.call(['brew', 'update'])
+  subprocess.call(['brew', 'cask', 'install'] + packages)
   
 packages = """
+- aria2
+- atop
+- byobu
+- curl
+- elinks
+- emacs
+- httpie
+- iftop
+- iptraf
+- iptraf-ng
+- ipcalc
+- jq
+- less
+- links
+- links2
+- lynx
+- most
+- netcat
+- nethogs
+- netpipes
+- nmap
+- rsync
+- rsyncrypto
+- rtorrent
+- screen
+- siege
+- socat
+- squid3
+- tmux
+- vim
+- vim-gtk
+- wget
 - ack-grep
 - ant
 - atop
