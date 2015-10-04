76b90fb42495b0c8e1ea2d72b44d40a303ac9db9
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-10-03 22:10:54 -0400

    Future statement must be first

--- ubuntu-monolith.py
+++ ubuntu-monolith.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import subprocess
 import os
 
-
-from __future__ import print_function
 
 class apt_gene(object):
     def __init__(self):
