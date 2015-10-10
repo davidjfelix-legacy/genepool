b264730d7015f10e7f9f9d56d5704270f83aa853
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-10-10 19:11:50 -0400

    Create apt.py

---
+++ apt.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+def install(*packages):
+    env = os.environ.copy()
+    env['DEBIAN_FRONTEND'] = "noninteractive"
+    subprocess.call(['sudo', '-E', 'apt-get', '-y', 'install'] + packages, env=env)
+    
+def update():
+    env = os.environ.copy()
+    env['DEBIAN_FRONTEND'] = "noninteractive"
+    subprocess.call(['sudo', '-E', 'apt-get', 'update'], env=env)
+    
+def upgrade():
+    env = os.environ.copy()
+    env['DEBIAN_FRONTEND'] = "noninteractive"
+    subprocess.call(['sudo', '-E', 'apt-get', 'upgrade'], env=env)
