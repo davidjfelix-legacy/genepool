4d4de16969439c71f0e9e15b32b26bd4b7310e8f
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-10-14 21:46:57 -0400

    Change simulated around for existing modules

--- Simulated_import.py
+++ Simulated_import.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # We're going to simulate how evolution_master should import things
 
-from genes import golang
-from genes import web_cli
+from genes import docker
+from genes import java
 
 # etc...
