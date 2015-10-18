cf452f749b779e06e9f93cfb68b5a41ddb8e02ef
David J Felix <felix.davidj@gmail.com>
2015-10-17 22:33:44 -0400

    Fix java's auto run and change simulated import

--- Simulated_import.py
+++ Simulated_import.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # We're going to simulate how evolution_master should import things
 
-from genes import docker
-from genes import java
+from importlib import import_module
+
+genes = ["docker"]
+
+for gene in genes:
+    mod = import_module("genes." + gene)
+    mod.main()
 
 # etc...
--- genes/java/__init__.py
+++ genes/java/__init__.py
@@ -1,5 +1,3 @@
 from . import main
 
 __all__ = ["main"]
-
-main.main()
