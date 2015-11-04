c709435d76e48a052ae4d4c7fe6be60516c9721d
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-11-03 22:38:48 -0500

    Update Simulated_import.py

--- Simulated_import.py
+++ Simulated_import.py
@@ -3,7 +3,7 @@
 
 from importlib import import_module
 
-genes = ["docker", "java", "web_cli_tools"]
+genes = ["docker", "java", "web_cli_tools", "intellij"]
 
 for gene in genes:
     mod = import_module("genes." + gene)
