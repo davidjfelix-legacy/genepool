38819b4d10d4e086881976c8df2b57e710e78b8d
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-11-12 13:51:47 -0500

    Remove cross compile in Linux... they broke

--- genes/golang/main.py
+++ genes/golang/main.py
@@ -9,15 +9,8 @@ def main():
     if is_ubuntu() or is_debian():
         apt.update()
         apt.install('golang')
-        # TODO: make this a runner and require a switch to enable this
-        apt.install('golang-go-darwin-amd64',
-                    'golang-go-freebsd-amd64',
-                    'golang-go-netbsd-amd64',
-                    'golang-go-windows-amd64')
-
     elif is_osx():
         brew.update()
         brew.install('go')
-
     else:
         pass
