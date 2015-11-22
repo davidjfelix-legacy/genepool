82954c638aa013a037125e0c9f167045f38da504
David J Felix <felix.davidj@gmail.com>
2015-11-22 17:29:37 -0500

    Migrate docker to new debian semantics

--- genes/docker/main.py
+++ genes/docker/main.py
@@ -1,15 +1,14 @@
 from genes.apt import commands as apt
 from genes.brew import commands as brew
-from genes import debian
-from genes.debian.traits import is_debian
+from genes.debian.traits import is_debian, get_distro, get_codename
 from genes.ubuntu.traits import is_ubuntu
 from genes.mac.traits import is_osx
 
 
 def main():
     if is_debian() or is_ubuntu():
-        repo = debian.traits.distribution.lower() + '-' + \
-               debian.traits.codename.lower()
+        repo = get_distro().lower() + '-' + \
+               get_codename().lower()
         apt.recv_keys('58118E89F3A912897C070ADBF76221572C52609D')
         apt.add_repo('deb', 'https://apt.dockerproject.org/repo', repo, 'main')
         apt.update()
