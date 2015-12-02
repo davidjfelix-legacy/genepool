8bd6b3d029c90109a602677ea4723a9a2670c12f
David J Felix <felix.davidj@gmail.com>
2015-12-02 15:43:54 -0500

    Refactor tests to possibly work (untested)

--- genes/apt/test_apt.py
+++ genes/apt/test_apt.py
@@ -1,127 +1,75 @@
 #!/usr/bin/env python
-try:
-    import mock
-except ImportError:
-    from unittest import mock
+from unittest import TestCase, mock
 
-from unittest import TestCase
-import genes
 from .commands import install, recv_keys, Config
 
 
 class TestAptCommands(TestCase):
-
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=False)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=False)
-    def test_install_package_on_non_apt_system(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
-        install('test')
-        self.assertFalse(genes.apt.commands.is_ubuntu())
-        self.assertFalse(genes.apt.commands.is_debian())
+    def test_install_package_on_non_apt_system(self, mock_popen):
+        install.__wrapped__('test')
         mock_popen.assert_not_called()
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=True)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=False)
-    def test_install_package_on_debian_system(
-            self, mock_is_ubuntu, mock_is_debian, mock_env_call):
+    def test_install_package_on_debian_system(self, mock_popen):
         """Test calling apt.install with one package"""
-        install('test')
-        self.assertTrue(genes.apt.commands.is_debian())
-        self.assertFalse(genes.apt.commands.is_ubuntu())
-        mock_env_call.assert_called_once_with(
-            ['apt-get', '-y', 'install', 'test'], env=Config.ENV)
+        install.__wrapped__('test')
+        mock_popen.assert_called_once_with(
+            ['apt-get', '-y', 'install', 'test'],
+            env=Config.ENV
+        )
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=False)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=True)
-    def test_install_package_on_ubuntu_system(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
+    def test_install_package_on_ubuntu_system(self, mock_popen):
         """Test calling apt.install with one package"""
-        install('test')
-        self.assertFalse(genes.apt.commands.is_debian())
-        self.assertTrue(genes.apt.commands.is_ubuntu())
+        install.__wrapped__('test')
         mock_popen.assert_called_once_with(
             ['apt-get', '-y', 'install', 'test'], env=Config.ENV)
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=True)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=True)
-    def test_install_packages(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
+    def test_install_packages(self, mock_popen):
         """Test calling apt install with multiple packages"""
-        install('test1', 'test2')
-        self.assertTrue(genes.apt.commands.is_debian())
-        self.assertTrue(genes.apt.commands.is_ubuntu())
+        install.__wrapped__('test1', 'test2')
         mock_popen.assert_called_once_with(
-            ['apt-get', '-y', 'install', 'test1', 'test2'], env=Config.ENV)
+            ['apt-get', '-y', 'install', 'test1', 'test2'],
+            env=Config.ENV
+        )
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=True)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=True)
-    def test_install_nothing(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
-        install()
-        self.assertTrue(genes.apt.commands.is_debian())
-        self.assertTrue(genes.apt.commands.is_ubuntu())
+    def test_install_nothing(self, mock_popen):
+        install.__wrapped()
         mock_popen.assert_not_called()
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=False)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=False)
-    def test_recv_key_on_non_apt_system(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
-        recv_keys()
-        self.assertFalse(genes.apt.commands.is_debian())
-        self.assertFalse(genes.apt.commands.is_ubuntu())
+    def test_recv_key_on_non_apt_system(self, mock_popen):
+        recv_keys.__wrapped__()
         mock_popen.assert_not_called()
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=True)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=False)
-    def test_recv_key_on_a_debian_system(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
-        recv_keys('test')
-        self.assertTrue(genes.apt.commands.is_debian())
-        self.assertFalse(genes.apt.commands.is_ubuntu())
+    def test_recv_key_on_a_debian_system(self, mock_popen):
+        recv_keys.__wrapped__('test')
         mock_popen.assert_called_once_with(
-            ['apt-key', 'adv', '--keyserver', 'hpk://pgp.mit.edu:80',
-             '--recv-keys', 'test'],
-            env=Config.ENV)
+            ['apt-key', 'adv', '--keyserver', 'hpk://pgp.mit.edu:80', '--recv-keys', 'test'],
+            env=Config.ENV
+        )
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=False)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=True)
-    def test_recv_key_on_an_ubuntu_system(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
-        recv_keys('test')
-        self.assertFalse(genes.apt.commands.is_debian())
-        self.assertTrue(genes.apt.commands.is_ubuntu())
+    def test_recv_key_on_an_ubuntu_system(self, mock_popen):
+        recv_keys.__wrapped__('test')
         mock_popen.assert_called_once_with(
-            ['apt-key', 'adv', '--keyserver', 'hpk://pgp.mit.edu:80',
-             '--recv-keys', 'test'],
-            env=Config.ENV)
+            ['apt-key', 'adv', '--keyserver', 'hpk://pgp.mit.edu:80', '--recv-keys', 'test'],
+            env=Config.ENV
+        )
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=True)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=True)
-    def test_recv_keys_on_an_apt_system(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
-        recv_keys('test1', 'test2')
-        self.assertTrue(genes.apt.commands.is_debian())
-        self.assertTrue(genes.apt.commands.is_ubuntu())
+    def test_recv_keys_on_an_apt_system(self, mock_popen):
+        recv_keys.__wrapped__('test1', 'test2')
         mock_popen.assert_called_once_with(
-            ['apt-key', 'adv', '--keyserver', 'hpk://pgp.mit.edu:80',
-             '--recv-keys', 'test1', 'test2'],
-            env=Config.ENV)
+            ['apt-key', 'adv', '--keyserver', 'hpk://pgp.mit.edu:80', '--recv-keys', 'test1', 'test2'],
+            env=Config.ENV
+        )
 
     @mock.patch('genes.apt.commands.Popen')
-    @mock.patch('genes.apt.commands.is_debian', return_value=True)
-    @mock.patch('genes.apt.commands.is_ubuntu', return_value=True)
-    def test_recv_keys_nothing(
-            self, mock_is_ubuntu, mock_is_debian, mock_popen):
-        recv_keys()
-        self.assertTrue(genes.apt.commands.is_debian())
-        self.assertTrue(genes.apt.commands.is_ubuntu())
+    def test_recv_keys_nothing(self, mock_popen):
+        recv_keys.__wrapped__()
         mock_popen.assert_not_called()
