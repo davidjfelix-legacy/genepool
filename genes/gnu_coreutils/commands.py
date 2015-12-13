c284e8c2faf32d02c7dcc1e59d7b422a3e091d7d
David J Felix <felix.davidj@gmail.com>
2015-12-13 12:05:03 -0500

    Migrate some files to new process model

--- genes/gnu_coreutils/commands.py
+++ genes/gnu_coreutils/commands.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
-import subprocess
+from genes.process.commands import run
+from genes.posix.traits import only_posix
 
-
+@only_posix()
 def chgrp(path, group):
-    # FIXME: don't use sudo
-    subprocess.call(['chgrp', group, path])
+    run(['chgrp', group, path])
 
 
+@only_posix()
 def chown(path, user):
-    # FIXME: don't use sudo
-    subprocess.call(['chown', user, path])
+    run(['chown', user, path])
 
 
+@only_posix()
 def mkdir(path, mode=None):
-    # FIXME: don't use sudo
     if mode:
-        subprocess.call(['mkdir', '-m', mode, path])
+        run(['mkdir', '-m', mode, path])
     else:
-        subprocess.call(['mkdir', path])
+        run(['mkdir', path])
+
+
+@only_posix()
+def useradd():
+    pass
+
+
+@only_posix()
+def usermod():
+    pass
