ad07f2bbd35bd1680ac48258e4df78947a402028
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-01-13 16:06:29 -0500

    Install build-essential

---
+++ genes/build_essential/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from genes.apt import commands as apt
+from genes.debian.traits import is_debian
+from genes.ubuntu.traits import is_ubuntu
+
+
+def main():
+    """Install packages considered necessary to build software"""
+    if is_debian() or is_ubuntu():
+        apt.install('build-essential')
+    else:
+        #FIXME: we need to support other distros soon
+        pass
