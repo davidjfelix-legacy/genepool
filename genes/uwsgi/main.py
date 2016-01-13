24e948230ba914ed7ad1ded08b3b8c2ed60093c3
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-01-13 16:21:38 -0500

    Create uwsgi installer

---
+++ genes/uwsgi/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from genes.apt import commands as apt
+from genes.debian.traits import is_debian
+from genes.ubuntu.traits import is_ubuntu
+
+
+def main():
+  """Install uWSGI either from system package or pip"""
+  # FIXME: handle pip case 
+  if is_ubuntu() or is_debian():
+    apt.install('uwsgi')
+  else:
+    # FIXME: handle other distros
