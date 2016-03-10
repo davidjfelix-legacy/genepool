b5d99aeddf3543881a95274bf6c30bce6e954b4e
David J Felix <felix.davidj@gmail.com>
2016-03-10 15:36:23 -0500

    Implement alpine apk stuff

--- genes/alpine/traits.py
+++ genes/alpine/traits.py
@@ -1,2 +1,38 @@
-def is_alpine():
-    pass
+from functools import wraps
+from typing import List, TypeVar, Callable, Tuple, Dict
+from typing import Optional
+
+from genes.lib.logging import log_warn, log_error
+from genes.lib.traits import ErrorLevel
+from genes.linux.traits import get_distro, get_version, get_codename
+
+T = TypeVar('T')
+
+
+def is_alpine(versions: Optional[List[str]] = None) -> bool:
+    is_version = True
+    if versions:
+        is_version = get_version() in versions or get_codename() in versions
+    return get_distro() == 'alpine' and is_version
+
+
+def only_alpine(error_level: ErrorLevel = ErrorLevel.warn, versions: Optional[List[str]] = None):
+    msg = "This function can only be run in alpine"
+
+    def wrapper(func: Callable[[Tuple, Dict], T]) -> Callable:
+        @wraps
+        def run_if_alpine(*args: Tuple, **kwargs: Dict) -> Optional[T]:
+            if is_alpine(versions=versions):
+                return func(*args, **kwargs)
+            elif error_level == ErrorLevel.warn:
+                log_warn(msg, func.__name__)
+                return None
+            elif error_level == ErrorLevel.error:
+                log_error(msg, func.__name__)
+                raise OSError(msg, func.__name__)
+            else:
+                return None
+
+        return run_if_alpine
+
+    return wrapper
--- genes/apk/commands.py
+++ genes/apk/commands.py
@@ -1,2 +1,17 @@
-def add(*packages):
-    pass
+import os
+
+from genes.alpine.traits import only_alpine
+from genes.lib.logging import log_error
+from genes.process.commands import get_env_run
+
+env_run = get_env_run(os.environ.copy())
+
+
+@only_alpine()
+def add(*packages) -> None:
+    if packages:
+        env_run('apk --no-cache add'.split() + list(packages))
+    else:
+        msg = 'Missing packages argument'
+        log_error(msg)
+        raise ValueError(msg)
