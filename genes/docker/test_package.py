29c3d555b9a399386d9de7d01853ce0fba057dd8
David J Felix <felix.davidj@gmail.com>
2016-04-15 11:15:23 -0400

    Define some tests i need to make

--- genes/docker/test_package.py
+++ genes/docker/test_package.py
@@ -2,4 +2,12 @@ from unittest import TestCase
 
 
 class TestDockerPkg(TestCase):
-    pass
+    def test_install_on_ubuntu_should_run_apt(self):
+        # FIXME: need to find a good way to set OS functions all at once
+        pass
+
+    def test_install_on_debian_should_run_apt(self):
+        pass
+
+    def test_install_on_osx_should_run_brew(self):
+        pass
