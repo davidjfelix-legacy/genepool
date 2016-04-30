896ee115bafb8be92c1116b49202d0bcd4b740f1
David J Felix <felix.davidj@gmail.com>
2016-04-30 10:22:08 -0400

    Add framework for tests

--- genes/adminuser/test_adminuser.py
+++ genes/adminuser/test_adminuser.py
@@ -2,4 +2,11 @@ from unittest import TestCase
 
 
 class AdminUserTestCase(TestCase):
-    pass
+    def test_install_on_ubuntu_should_run_cmds(self):
+        pass
+
+    def test_install_on_debian_should_run_cmds(self):
+        pass
+
+    def test_install_on_osx_should_run_dscl(self):
+        pass
